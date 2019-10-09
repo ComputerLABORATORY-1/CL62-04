79de1db62f07637cc7cb2ae9109d8b2523b352bb
muaywspkt <61030050@kmitl.ac.th>
2019-10-10 01:38:02 +0700

    add

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,23 +7,12 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            String str;
-            double val2;
-            Console.WriteLine("Please Enter your NAME:");
-            str = Console.ReadLine();
-            Console.WriteLine("Your name is: " + str);
 
-            Console.WriteLine("Please Enter your LASTNAME:");
-            str = Console.ReadLine();
-            Console.WriteLine("Your lastname is: " + str);
 
-            Console.WriteLine("Please Enter your ID:");
-            int val1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your ID is: "+val1);
-
-            Console.WriteLine("Please Enter your GPA:");
-            double Val2 = Convert.ToInt64(Console.ReadLine());
-            Console.WriteLine("Your ID is: " + Val2);
+            Console.WriteLine("Data type : int");
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + int.MinValue);
+            Console.WriteLine("Maximum Value :" + int.MaxValue);
 
 
 
